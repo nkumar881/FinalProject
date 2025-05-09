bc8dd591bf5c5df7f76d62949ec9eace0acd22b8
nkumar881 <nkumar88@iastate.edu>
2025-05-09 18:48:03 -0500

    Update uart.c

--- uart.c
+++ uart.c
@@ -14,9 +14,11 @@
 #include "lcd.h"
 
 
-int DIR;
-int ANGLE;
 
+int ANGLE;
+/*
+ * this method is used to initallize the servo motor for the cybot that is used to scan
+ */
 void servo_init(void)
 {
     SYSCTL_RCGCGPIO_R |= 0x02;
@@ -44,157 +46,19 @@ void servo_init(void)
     TIMER1_CTL_R |= 0x100;
 
     ANGLE = 90;
-    DIR = 0;
+
 }
 
-void servo_move(uint16_t degrees)
+void servoMove(uint16_t degrees)
 {
-    int leftBound = 291022;
+    int leftBound = 291022;//0 degrees
 
-    int rightBound = 319822;
+    int rightBound = 319822;// 180 degrees
 
     uint32_t lowPulse = rightBound - ((rightBound - leftBound) / 180) * degrees;
 
     TIMER1_TBMATCHR_R = (lowPulse & 0xFFFF);
-    TIMER1_TBPMR_R = (lowPulse >> 16);
     ANGLE = degrees;
 }
 
-void button_move(void)
-{
-    int temp = 0;
-    while (temp == 0)
-    {
-        temp = button_getButton();
-        timer_waitMillis(100);
-    }
-    if (temp == 1)
-    {
-        if (DIR == 0)
-        {
-            if (ANGLE >= 179)
-            {
-                servo_move(180);
-            }
-            else
-            {
-                ANGLE += 1;
-                servo_move(ANGLE);
-            }
-        }
-        else
-        {
-            if (ANGLE <= 1)
-            {
-                servo_move(0);
-            }
-            else
-            {
-                ANGLE -= 1;
-                servo_move(ANGLE);
-            }
-        }
-    }
-    else if (temp == 2)
-    {
-        if (DIR == 0)
-        {
-            if (ANGLE >= 175)
-            {
-                servo_move(180);
-            }
-            else
-            {
-                ANGLE += 5;
-                servo_move(ANGLE);
-            }
-        }
-        else
-        {
-            if (ANGLE <= 5)
-            {
-                servo_move(0);
-            }
-            else
-            {
-                ANGLE -= 5;
-                servo_move(ANGLE);
-            }
-        }
-    }
-    else if (temp == 3)
-    {
-        if (DIR == 0)
-        {
-            DIR = 1;
-        }
-        else
-        {
-            DIR = 0;
-        }
-    }
-    else if (temp == 4)
-    {
-        if (DIR == 0)
-        {
-            servo_move(175);
-        }
-        else
-        {
-            servo_move(5);
-        }
-    }
-    temp = 0;
-    char ang[10];
-    sprintf(ang, "%d", ANGLE);
-    lcd_printf(ang);
-}
-
-void calibrate(void)
-{
-    int x = 0;
-    uint32_t value;
-    uint32_t value2;
-
-    TIMER1_TBMATCHR_R = 0xE200;
-    TIMER1_TBPMR_R = 0x4;
-    int count = 0;
-    char array[40];
-    lcd_printf(
-            "Button1: move left\nButton2: move right\nButton3: get value1\nButton4: get value2");
-    while (x == 0)
-    {
-        int y = 0;
-        while (y == 0)
-        {
-            y = button_getButton();
-            timer_waitMillis(100);
-        }
-        if (y == 1)
-        {
-            count++;
-            servo_move(count);
-        }
-        else if (y == 2)
-        {
-            count--;
-            servo_move(count);
-        }
-        else if (y == 3)
-        {
-            value = TIMER1_TBMATCHR_R;
-            value += 262144;
-            sprintf(array, "Value 1: %d\n", value);
-            lcd_printf(array);
-        }
-        else if (y == 4)
-        {
-            value2 = TIMER1_TBMATCHR_R;
-            value2 += 262144;
-            sprintf(array, "Value 1: %d\nValue 2: %d", value, value2);
-            lcd_printf(array);
-            x = 1;
-        }
-    }
-}
 
