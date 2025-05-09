bbf2cfcd0eb641831ba89d486a0043a9c0319f70
nkumar881 <nkumar88@iastate.edu>
2025-05-09 18:03:08 -0500

    Update uart.c

--- uart.c
+++ uart.c
@@ -1,87 +1,56 @@
 /*
-*
-*   uart.c
-*
-*
-*
-*   @author
-*   @date
-*/
+ *   uart.c
+ *   @author Mitch Folwer, Nikhil Kumar, Adam, Brandon Beaver
+ *   @date
+ */
 
 #include <inc/tm4c123gh6pm.h>
 #include <stdint.h>
 #include "uart.h"
 
-void uart_init(void){
-	//TODO
-  //enable clock to GPIO port B
-  SYSCTL_RCGCGPIO_R |= 0x02;
-
-  //enable clock to UART1
-  SYSCTL_RCGCUART_R |= 0x02;
-
-  //wait for GPIOB and UART1 peripherals to be ready
-  while ((SYSCTL_PRGPIO_R & 0x02) == 0) {}
-  while ((SYSCTL_PRUART_R & 0x02) == 0) {}
-
-  //enable alternate functions on port B pins
-  GPIO_PORTB_AFSEL_R |= 0x03;
-
-  //enable digital functionality on port B pins
-  GPIO_PORTB_DEN_R |= 0x03;
-
-  //enable UART1 Rx and Tx on port B pins
-  GPIO_PORTB_PCTL_R &= ~0x000000FF;
-  GPIO_PORTB_PCTL_R |= 0x00000011;
- // cyBot_uart_init_last_half();//this should work
-  //calculate baud rate
-  uint16_t iBRD = 8; //use equations
-  uint16_t fBRD = 44; //use equations
-
-  //turn off UART1 while setting it up
-  UART1_CTL_R &= ~0x0001;
-
-  //set baud rate
-  //note: to take effect, there must be a write to LCRH after these assignments
-  UART1_IBRD_R = iBRD;
-  UART1_FBRD_R = fBRD;
-
-  //set frame, 8 data bits, 1 stop bit, no parity, no FIFO
-  //note: this write to LCRH must be after the BRD assignments
-  UART1_LCRH_R = 0x60;
-
-  //use system clock as source
-  //note from the datasheet UARTCCC register description:
-  //field is 0 (system clock) by default on reset
-  //Good to be explicit in your code
-  UART1_CC_R = 0x0;
-
-  //re-enable UART1 and also enable RX, TX (three bits)
-  //note from the datasheet UARTCTL register description:
-  //RX and TX are enabled by default on reset
-  //Good to be explicit in your code
-  //Be careful to not clear RX and TX enable bits
-  //(either preserve if already set or set them)
-  UART1_CTL_R = 0x301;
-
+void uart_init(void)
+{
+    SYSCTL_RCGCGPIO_R |= 0x02; //clock to port B
+    //enable clock to UART1
+    SYSCTL_RCGCUART_R |= 0x02;  //enables clock to uart 1
+    while ((SYSCTL_PRGPIO_R & 0x02) == 0)
+    {
+    }
+    while ((SYSCTL_PRUART_R & 0x02) == 0)
+    {
+    }  //amkes sure the GPIO and UART peripherals are ready
+    GPIO_PORTB_AFSEL_R |= 0x03;  //enables alt functionality
+    GPIO_PORTB_DEN_R |= 0x03;  //digital enable
+    GPIO_PORTB_PCTL_R &= ~0x000000FF;
+    GPIO_PORTB_PCTL_R |= 0x00000011;
+    uint16_t iBRD = 8;
+    uint16_t fBRD = 44; //use equations to get the same
+    UART1_CTL_R &= ~0x0001;
+    UART1_IBRD_R = iBRD;
+    UART1_FBRD_R = fBRD;
+    UART1_LCRH_R = 0x60;
+    UART1_CC_R = 0x0;
+    UART1_CTL_R = 0x301;
 }
 
-void uart_sendChar(char data){
-	while (UART1_FR_R & 0x20);  // busy wait loop
-	UART1_DR_R = data;
+void uart_sendChar1(char data)
+{
+    while (UART1_FR_R & 0x20); //busy wait loop to implement the same
+    UART1_DR_R = data;
 }
 
-char uart_receive(void){
-	while (UART1_FR_R & 0x10);
-	return (char)(UART1_DR_R & 0xFF);
+char uart_receive(void)
+{
+    while (UART1_FR_R & 0x10);//blocking uart function
+    return (char) (UART1_DR_R & 0xFF);
 }
 
-void uart_sendStr(const char *data){
-	//TODO for reference see lcd_puts from lcd.c file
+void uart_sendStr1(const char *data)
+{
     int i;
     int length = strlen(data);
-    for (i = 0;i<length;i++)
+    for (i = 0; i < length; i++)
     {
-        uart_sendChar (data[i]);
+        uart_sendChar1(data[i]);
     }
 }
