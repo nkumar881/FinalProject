d04f84c4c210a9d08dd3caf2e9f8607828363369
nkumar881 <nkumar88@iastate.edu>
2025-05-09 18:40:24 -0500

    Update uart.c

--- uart.c
+++ uart.c
@@ -1,94 +1,200 @@
 /*
- *
- *   uart-interrupt.c
- *
- *   @author Mitch Folwer, Nikhil Kumar, Adam, Brandon Beaver
- *   @date
+ * @author Mitch Folwer, Nikhil Kumar, Adam, Brandon Beaver
  */
 
-#include <inc/tm4c123gh6pm.h>
+#include "servo.h"
 #include <stdint.h>
-#include "uart-interrupt.h"
+#include <stdbool.h>
+#include <inc/tm4c123gh6pm.h>
+#include "driverlib/interrupt.h"
+#include "button.h"
 
-// These variables are declared as examples for your use in the interrupt handler.
-volatile char command_byte = -1; // byte value for special character used as a command
-volatile int command_flag = 0; // flag to tell the main program a special command was received
-volatile char toPrint = 0;
+#include "Timer.h"
+#include "button.h"
+#include "lcd.h"
 
-void uart_interrupt_init(void)
+
+int DIR;
+int ANGLE;
+
+void servo_init(void)
 {
     SYSCTL_RCGCGPIO_R |= 0x02;
-    SYSCTL_RCGCUART_R |= 0x02;
     while ((SYSCTL_PRGPIO_R & 0x02) == 0)
     {
     };
-    while ((SYSCTL_PRUART_R & 0x02) == 0)
+    SYSCTL_RCGCTIMER_R |= 0x02;
+    while ((SYSCTL_PRTIMER_R & 0x02) == 0)
     {
     };
-    GPIO_PORTB_DEN_R |= 0x03;
-    GPIO_PORTB_AFSEL_R |= 0x03;
-    GPIO_PORTB_PCTL_R &= ~0x000000FF;
-    GPIO_PORTB_PCTL_R |= 0x00000011;
-    uint16_t iBRD = 8;
-    uint16_t fBRD = 44;
-    UART1_CTL_R &= ~0x0001;
-    UART1_IBRD_R = iBRD;
-    UART1_FBRD_R = fBRD;
-    UART1_LCRH_R = 0x60;
-    UART1_CC_R = 0x0;
-    UART1_ICR_R |= 0b00010000;
-    UART1_IM_R |= 0x00000010;
-    NVIC_PRI1_R = (NVIC_PRI1_R & 0xFF0FFFFF) | 0x00200000;
-    NVIC_EN0_R |= (1 << 6);
-    IntRegister(INT_UART1, UART1_Handler);
-    IntMasterEnable();
-    UART1_CTL_R |= (0x00000001 | 0x00000200 | 0x00000100);
+    GPIO_PORTB_DEN_R |= 0x20;
+    GPIO_PORTB_DIR_R |= 0x20;
 
+    GPIO_PORTB_AFSEL_R |= 0x20;
+    GPIO_PORTB_PCTL_R = (GPIO_PORTB_PCTL_R & ~0x00700000) | 0x00700000;
+
+    TIMER1_CTL_R &= ~(0x100);
+    TIMER1_CFG_R = 0x00000004;
+    TIMER1_TBMR_R |= 0b1010;
+    TIMER1_CTL_R &= 0xBFFF;
+    TIMER1_TBPR_R |= 0x4;
+    TIMER1_TBILR_R |= 0xE200;
+    TIMER1_TBMATCHR_R = 0xA90E;
+    TIMER1_TBPMR_R = 0x4;
+    TIMER1_CTL_R |= 0x100;
+
+    ANGLE = 90;
+    DIR = 0;
 }
 
-void uart_sendChar(char data)
+void servo_move(uint16_t degrees)
 {
+    int leftBound = 291022;
 
-    while ((UART1_FR_R & UART_FR_TXFF) != 0);
-    UART1_DR_R = data;
+    int rightBound = 319822;
+
+    uint32_t lowPulse = rightBound - ((rightBound - leftBound) / 180) * degrees;
+
+    TIMER1_TBMATCHR_R = (lowPulse & 0xFFFF);
+    TIMER1_TBPMR_R = (lowPulse >> 16);
+    ANGLE = degrees;
 }
 
-
-void uart_sendStr(const char *data)
+void button_move(void)
 {
-    int i;
-    int length = strlen(data);
-    for (i = 0; i < length; i++)
+    int temp = 0;
+    while (temp == 0)
     {
-        uart_sendChar(data[i]);
+        temp = button_getButton();
+        timer_waitMillis(100);
     }
-}
-
-// Interrupt handler for receive interrupts
-void UART1_Handler(void)
-{
-    char byte_received;
-
-    if (UART1_MIS_R & 0x10)
+    if (temp == 1)
     {
-
-        UART1_ICR_R |= 0b00010000;
-
-        byte_received = (char) (UART1_DR_R & 0xFF);
-        uart_sendChar(byte_received);
-
-
-        if (byte_received == '\r')
+        if (DIR == 0)
         {
-
-            uart_sendChar('\n');
+            if (ANGLE >= 179)
+            {
+                servo_move(180);
+            }
+            else
+            {
+                ANGLE += 1;
+                servo_move(ANGLE);
+            }
         }
         else
         {
-            if (byte_received == command_byte)
+            if (ANGLE <= 1)
             {
-                command_flag = 1;
+                servo_move(0);
+            }
+            else
+            {
+                ANGLE -= 1;
+                servo_move(ANGLE);
             }
         }
     }
+    else if (temp == 2)
+    {
+        if (DIR == 0)
+        {
+            if (ANGLE >= 175)
+            {
+                servo_move(180);
+            }
+            else
+            {
+                ANGLE += 5;
+                servo_move(ANGLE);
+            }
+        }
+        else
+        {
+            if (ANGLE <= 5)
+            {
+                servo_move(0);
+            }
+            else
+            {
+                ANGLE -= 5;
+                servo_move(ANGLE);
+            }
+        }
+    }
+    else if (temp == 3)
+    {
+        if (DIR == 0)
+        {
+            DIR = 1;
+        }
+        else
+        {
+            DIR = 0;
+        }
+    }
+    else if (temp == 4)
+    {
+        if (DIR == 0)
+        {
+            servo_move(175);
+        }
+        else
+        {
+            servo_move(5);
+        }
+    }
+    temp = 0;
+    char ang[10];
+    sprintf(ang, "%d", ANGLE);
+    lcd_printf(ang);
 }
+
+void calibrate(void)
+{
+    int x = 0;
+    uint32_t value;
+    uint32_t value2;
+
+    TIMER1_TBMATCHR_R = 0xE200;
+    TIMER1_TBPMR_R = 0x4;
+    int count = 0;
+    char array[40];
+    lcd_printf(
+            "Button1: move left\nButton2: move right\nButton3: get value1\nButton4: get value2");
+    while (x == 0)
+    {
+        int y = 0;
+        while (y == 0)
+        {
+            y = button_getButton();
+            timer_waitMillis(100);
+        }
+        if (y == 1)
+        {
+            count++;
+            servo_move(count);
+        }
+        else if (y == 2)
+        {
+            count--;
+            servo_move(count);
+        }
+        else if (y == 3)
+        {
+            value = TIMER1_TBMATCHR_R;
+            value += 262144;
+            sprintf(array, "Value 1: %d\n", value);
+            lcd_printf(array);
+        }
+        else if (y == 4)
+        {
+            value2 = TIMER1_TBMATCHR_R;
+            value2 += 262144;
+            sprintf(array, "Value 1: %d\nValue 2: %d", value, value2);
+            lcd_printf(array);
+            x = 1;
+        }
+    }
+}
+
